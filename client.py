62e57bf2dc1ffd84e54335db253f4362ea697ca5
gokul-gk-02 <55867929+gokul-gk-02@users.noreply.github.com>
2021-12-10 12:54:28 +0530

    Add files via upload

---
+++ client.py
@@ -0,0 +1,130 @@
+import socket
+import threading
+import tkinter
+import tkinter.scrolledtext
+from tkinter import simpledialog
+import time
+import os
+
+BUFFER_SIZE = 64
+FORMAT = 'utf-8'
+
+HOST = "192.168.1.203"
+PORT = 5050
+
+class Client:
+
+    def __init__(self, host, port):
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.sock.connect((host, port))
+
+        msg = tkinter.Tk()
+        msg.withdraw()
+
+        self.nickname = simpledialog.askstring("Nickname", "Please choose a nickname", parent = msg)
+
+        self.gui_done = False
+        self.running = True
+
+        gui_thread = threading.Thread(target = self.gui_loop)
+        receive_thread = threading.Thread(target = self.receive)
+
+        gui_thread.start()
+        time.sleep(0.5)
+        receive_thread.start()
+
+    def gui_loop(self):
+        self.win = tkinter.Tk()
+        self.win.configure(bg= "lightgray")
+
+        self.chat_label = tkinter.Label(self.win, text = "Chat: ", bg="lightgray")
+        self.chat_label.config(font=("Arial", 12))
+        self.chat_label.pack(padx=20, pady=5)
+
+        self.text_area = tkinter.scrolledtext.ScrolledText(self.win)
+        self.text_area.pack(padx=20, pady=5)
+        self.text_area.config(state='disabled')
+
+        self.msg_label = tkinter.Label(self.win, text = "Message: ", bg="lightgray")
+        self.msg_label.config(font=("Arial", 12))
+        self.msg_label.pack(padx=20, pady=5)
+
+        self.input_area = tkinter.Text(self.win, height = 3)
+        self.input_area.pack(padx=20, pady=5)
+
+        self.send_button = tkinter.Button(self.win, text = "Send", command=self.write)
+        self.send_button.config(font = ("Arial", 12))
+        self.send_button.pack(padx=20, pady=5)
+
+        self.gui_done = True
+
+        self.win.protocol("VM_DELETE_WINDOW", self.stop)
+
+        self.win.mainloop()
+
+    def write(self):
+        message = f"{self.input_area.get('1.0', 'end')}"
+        self.sock.send(message.encode(FORMAT))
+        self.input_area.delete('1.0', 'end')
+        if message.startswith("!DISCONNECT"):
+            print("Disconnecting from the chat room...")
+            self.stop()
+        elif message.startswith("!FILE"):
+            print(f"Sending the file {message.split()[-1]}...")
+            filename = message.split()[-1]
+
+            filesize = os.path.getsize(filename)
+            file_length = str(filesize).encode(FORMAT)
+            file_length += b' ' * (BUFFER_SIZE - len(file_length))
+            self.sock.send(file_length)
+            with open(filename, "rb") as f:
+                while True:
+                    bytes_read = f.read(filesize)
+                    if not bytes_read:
+                        break
+                    self.sock.sendall(bytes_read)
+
+            print(f"File {filename} sent successfully")
+        
+
+    def stop(self):
+        self.running = False
+        self.win.destroy()
+        self.sock.close()
+        exit(0)
+
+    def receive(self):
+        while self.running:
+            try:
+                message = self.sock.recv(1024).decode(FORMAT)
+                if message == 'NICK':
+                    self.sock.send(self.nickname.encode(FORMAT))
+                elif message.startswith('!FILE'):
+                    print(f"Preparing to receive the file...")
+                    filename = message.split()[-1]
+                    filesize = self.sock.recv(BUFFER_SIZE).decode()
+
+                    filename = os.path.basename(filename)
+
+                    filesize = int(filesize)
+                    with open(filename, "wb") as f:
+                        bytes_read = self.sock.recv(filesize)
+                        f.write(bytes_read)
+                    
+                    print(f"File: {filename} received successfully")
+
+                else:
+                    if self.gui_done:
+                        self.text_area.config(state='normal')
+                        self.text_area.insert('end', message)
+                        self.text_area.yview('end')
+                        self.text_area.config(state='disabled')
+
+            except ConnectionAbortedError:
+                break
+            except:
+                print("Error")
+                self.sock.close()
+                break
+
+client = Client(HOST, PORT)
